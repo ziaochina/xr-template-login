a9d60ba875b52b0f2db60f7a1da88bdd6d29e006
ziaochina <ziaochina@gmail.com>
2017-07-13 18:01:28 +0800

    zlj

--- api.js
+++ api.js
@@ -21,12 +21,12 @@ export function getMeta() {
 				component: 'Form',
 				className:'content-form',
 				children: [{
-					name: 'title',
+					name: 'item1',
 					component: 'Form.Item',
 					className: 'content-form-title',
 					children: 'Login'
 				}, {
-					name: 'userItem',
+					name: 'item2',
 					component: 'Form.Item',
 					children:[{
 						name: 'user',
@@ -40,9 +40,8 @@ export function getMeta() {
 							type:'user',
 						}
 					}]
-
 				},{
-					name: 'passwordItem',
+					name: 'item3',
 					component: 'Form.Item',
 					children:[{
 						name: 'password',
@@ -57,15 +56,36 @@ export function getMeta() {
 							type:'lock',
 						}
 					}]
-
 				},{
-					name:'loginBtnItem',
+					name: 'item4',
+					component: 'Form.Item',
+					children:[{
+						name:'remember',
+						component:'Checkbox',
+						children: 'Remember me'
+					},{
+						name:'forgot',
+						component:'::a',
+						style:{float:'right'},
+						children:'Forgot password'
+					}]
+				},{
+					name:'item5',
 					component:'Form.Item',
 					children:[{
 						name: 'loginBtn',
 						component: 'Button',
-						children: 'Login',
+						type:'primary',
+						children: 'Log in',
 						onClick:'{{$login}}'
+					}]
+				},{
+					name: 'item6',
+					component: 'Form.Item',
+					children:[{
+						name:'register',
+						component:'::a',
+						children:'Register now!'
 					}]
 				}]
 			}, {
